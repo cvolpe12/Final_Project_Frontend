64bcfdad2d96d7258c3604c65fbabafb0056cb5e
cvolpe12 <christopher.volpe12@gmail.com>
2019-04-05 14:22:18 -0400

    can conditionally render team

--- src/containers/UserTeamContainer.js
+++ src/containers/UserTeamContainer.js
@@ -1,13 +1,23 @@
 import React from "react"
 import { connect } from "react-redux"
+import Team from "../components/Team"
 
 const UserTeamContainer = (props) => {
 
+  const renderTeam = () => {
+    console.log(props.team);
+    return props.team.map(player => {
+      return <Team key={player.id} player={player}/>
+    })
+  }
 
   console.log(props)
   return (
   <div className="">
   user team
+    <div className="player-list">
+      {renderTeam()}
+    </div>
   </div>
   )
 }
