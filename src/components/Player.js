01ea296c22b2147d10080d64259de0f680dc7a63
cvolpe12 <christopher.volpe12@gmail.com>
2019-04-15 18:21:59 -0400

    fixed bug where user can add players even if positions are filled

--- src/components/Player.js
+++ src/components/Player.js
@@ -8,25 +8,126 @@ class Player extends React.Component {
 // else alert player on team
   playerOnTeam = () => {
     // console.log(this.props.player);
-    if (Object.values(this.props.team).indexOf(this.props.player) > -1){
+    console.log(this.props.currentTeam);
+    // if the player is in the props.team then it is true
+    if (this.positionFilled(this.props.player) && Object.values(this.props.team).indexOf(this.props.player) > -1){
+
       return true
-    }
-    else {
+    } else {
       return false
     }
-    // console.log(this.props.team.includes(this.props.player));
-    // return this.props.team.includes(this.props.player)
-    // Object.keys(this.props.team).forEach( position => {
-    //   console.log(position);
-    //   console.log(this.props.team[position] === this.props.player);
-    //   return this.props.team[position] === this.props.player
-    // })
   }
 
+  positionFilled = () => {
+    let team = this.props.team
+    // debugger
+    console.log("running position fillled");
+    // if the player is on the roster then i should not do anything
+    // if the player is not on the roster then i should check the positions
+    // if the player's position is empty i should add to position
+    // if the player's position is full i should check extra
+    // if the extra position is empty i should add to extra
+    // if the extra position is full i cannot add player
+    if (Object.values(this.props.team).indexOf(this.props.player) === -1) {
+      // means that player is not on the team
+      switch (this.props.player.position) {
+        case "C":
+          if (team.catcher) {
+            if (team.extra) {
+              alert("Position is filled.")
+              return true
+            } else {
+              return false
+            }
+          } else {
+            return false
+          }
+        case "1B":
+          if (team.first) {
+            if (team.extra) {
+              alert("Position is filled.")
+              return true
+            } else {
+              return false
+            }
+          } else {
+            return false
+          }
+        case "2B":
+          if (team.second) {
+            if (team.extra) {
+              alert("Position is filled.")
+              return true
+            } else {
+              return false
+            }
+          } else {
+            return false
+          }
+        case "3B":
+          if (team.third) {
+            if (team.extra) {
+              alert("Position is filled.")
+              return true
+            } else {
+              return false
+            }
+          } else {
+            return false
+          }
+        case "SS":
+          if (team.short) {
+            if (team.extra) {
+              alert("Position is filled.")
+              return true
+            } else {
+              return false
+            }
+          } else {
+            return false
+          }
+        case "OF":
+          if (team.out1) {
+            if (team.out2) {
+              if (team.out3) {
+                if (team.extra) {
+                  alert("Position is filled.")
+                  return true
+                } else {
+                  return false
+                }
+              } else {
+                return false
+              }
+            } else {
+              return false
+            }
+          } else {
+            return false
+          }
+        case "DH":
+          if (team.extra) {
+              alert("Position is filled.")
+              return true
+            } else {
+              return false
+            }
+        default:
+          return null
+      }
+    } else {
+      alert("Player is on your team.")
+      return true
+    }
+  }
+
+// creates the Draft object
   addToTeam = e => {
-    if (this.playerOnTeam() === false) {
-      e.preventDefault()
-      console.log(this.props.currentTeam);
+    console.log("running event handler");
+    // debugger
+    e.preventDefault()
+    if (this.positionFilled() === false) {
+      // console.log(this.props.currentTeam);
       // console.log(this.props.player);
       // console.log(e.target.value)
       // this.props.addPlayerToTeam(this.props.player)
@@ -50,7 +151,7 @@ class Player extends React.Component {
 
       })
     } else {
-      alert("Player is on your team.")
+      return null
     }
   }
 
@@ -70,6 +171,7 @@ class Player extends React.Component {
     .then(res => res.json())
   }
 
+// way to check if user has all position is filled
   addPlayerToTeam = player => {
     let team = this.props.team
     // condition ? exprT : exprF
@@ -77,7 +179,7 @@ class Player extends React.Component {
       case "C":
         if (team.catcher) {
           if (team.extra) {
-            alert("Position is filled.")
+            return null
           } else {
             this.props.addExtra(player)
             this.setPlayerToExtra(player)
@@ -89,7 +191,7 @@ class Player extends React.Component {
       case "1B":
         if (team.first) {
           if (team.extra) {
-            alert("Position is filled.")
+            return null
           } else {
             this.props.addExtra(player)
             this.setPlayerToExtra(player)
@@ -101,7 +203,7 @@ class Player extends React.Component {
       case "2B":
         if (team.second) {
           if (team.extra) {
-            alert("Position is filled.")
+            return null
           } else {
             this.props.addExtra(player)
             this.setPlayerToExtra(player)
@@ -113,7 +215,7 @@ class Player extends React.Component {
       case "3B":
         if (team.third) {
           if (team.extra) {
-            alert("Position is filled.")
+            return null
           } else {
             this.props.addExtra(player)
             this.setPlayerToExtra(player)
@@ -125,7 +227,7 @@ class Player extends React.Component {
       case "SS":
         if (team.short) {
           if (team.extra) {
-            alert("Position is filled.")
+            return null
           } else {
             this.props.addExtra(player)
             this.setPlayerToExtra(player)
@@ -139,7 +241,7 @@ class Player extends React.Component {
           if (team.out2) {
             if (team.out3) {
               if (team.extra) {
-                alert("Position is filled.")
+                return null
               } else {
                 this.props.addExtra(player)
                 this.setPlayerToExtra(player)
@@ -156,7 +258,7 @@ class Player extends React.Component {
         break;
         case "DH":
           if (team.extra) {
-            alert("Position is filled.")
+            return null
           } else {
             this.props.addExtra(player)
             this.setPlayerToExtra(player)
@@ -211,7 +313,8 @@ function mapDispatchToProps(dispatch){
     addOF1: (player) => {dispatch({type: "SELECT_OUTFIELD1", payload: player })},
     addOF2: (player) => {dispatch({type: "SELECT_OUTFIELD2", payload: player })},
     addOF3: (player) => {dispatch({type: "SELECT_OUTFIELD3", payload: player })},
-    addExtra: (player) => {dispatch({type: "SELECT_EXTRA", payload: player })}
+    addExtra: (player) => {dispatch({type: "SELECT_EXTRA", payload: player })},
+    addPlayerToCurrentTeam: (player) => {dispatch({type: "Add_PLAYER_CURRENT_TEAM", payload: player })}
   }
 }
 
