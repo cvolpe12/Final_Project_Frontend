b8ca2243ec0c16d6e027d40bfd015262f5a13169
cvolpe12 <christopher.volpe12@gmail.com>
2019-04-16 14:53:49 -0400

    can calculate score

--- src/components/LeagueTeams.js
+++ src/components/LeagueTeams.js
@@ -26,37 +26,139 @@ class LeagueTeams extends React.Component {
     })
   }
 
+  renderScore = player => {
+    let score = player.game_hits + (player.game_doubles * 2) + (player.game_triples * 3) + (player.game_home_runs * 4) + player.game_rbi + player.game_runs + player.game_walks
+    return score
+  }
+
   findCatcher = (team) => {
     let player = team.players.find(player => player.position === "C" && player.extra_player === false)
-    return <p>C: {player.name}</p>
+    return (
+      <div>
+        <p>C: {player.name}</p>
+        <p>Points: {this.renderScore(player)}</p>
+        <p class="meta">
+        Hits: {player.game_hits} |
+        Doubles: {player.game_doubles} |
+        Triples: {player.game_triples} |
+        HR: {player.game_home_runs} |
+        RBI: {player.game_rbi} |
+        Runs: {player.game_runs} |
+        Walks: {player.game_walks}
+        </p>
+      </div>
+    )
   }
   findFirst = (team) => {
     let player = team.players.find(player => player.position === "1B" && player.extra_player === false)
-    return <p>1B: {player.name}</p>
+    return (
+      <div>
+        <p>1B: {player.name}</p>
+        <p>Points: {this.renderScore(player)}</p>
+        <p class="meta">
+        Hits: {player.game_hits} |
+        Doubles: {player.game_doubles} |
+        Triples: {player.game_triples} |
+        HR: {player.game_home_runs} |
+        RBI: {player.game_rbi} |
+        Runs: {player.game_runs} |
+        Walks: {player.game_walks}
+        </p>
+      </div>
+    )
   }
   findSecond = (team) => {
     let player = team.players.find(player => player.position === "2B" && player.extra_player === false)
-    return <p>2B: {player.name}</p>
+    return (
+      <div>
+        <p>2B: {player.name}</p>
+        <p>Points: {this.renderScore(player)}</p>
+        <p class="meta">
+        Hits: {player.game_hits} |
+        Doubles: {player.game_doubles} |
+        Triples: {player.game_triples} |
+        HR: {player.game_home_runs} |
+        RBI: {player.game_rbi} |
+        Runs: {player.game_runs} |
+        Walks: {player.game_walks}
+        </p>
+      </div>
+    )
   }
   findThird = (team) => {
     let player = team.players.find(player => player.position === "3B" && player.extra_player === false)
-    return <p>3B: {player.name}</p>
+    return (
+      <div>
+        <p>3B: {player.name}</p>
+        <p>Points: {this.renderScore(player)}</p>
+        <p class="meta">
+        Hits: {player.game_hits} |
+        Doubles: {player.game_doubles} |
+        Triples: {player.game_triples} |
+        HR: {player.game_home_runs} |
+        RBI: {player.game_rbi} |
+        Runs: {player.game_runs} |
+        Walks: {player.game_walks}
+        </p>
+      </div>
+    )
   }
   findShort = (team) => {
     let player = team.players.find(player => player.position === "SS" && player.extra_player === false)
-    return <p>SS: {player.name}</p>
+    return (
+      <div>
+        <p>SS: {player.name}</p>
+        <p>Points: {this.renderScore(player)}</p>
+        <p class="meta">
+        Hits: {player.game_hits} |
+        Doubles: {player.game_doubles} |
+        Triples: {player.game_triples} |
+        HR: {player.game_home_runs} |
+        RBI: {player.game_rbi} |
+        Runs: {player.game_runs} |
+        Walks: {player.game_walks}
+        </p>
+      </div>
+    )
   }
   findOutfield = (team) => {
     let outfielders = team.players.filter(player => player.position === "OF" && player.extra_player === false)
     return outfielders.map(player => {
-      return <p>OF: {player.name}</p>
+      return (
+        <div>
+          <p>OF: {player.name}</p>
+          <p>Points: {this.renderScore(player)}</p>
+          <p class="meta">
+          Hits: {player.game_hits} |
+          Doubles: {player.game_doubles} |
+          Triples: {player.game_triples} |
+          HR: {player.game_home_runs} |
+          RBI: {player.game_rbi} |
+          Runs: {player.game_runs} |
+          Walks: {player.game_walks}
+          </p>
+        </div>
+      )
     })
   }
-
   findExtra = (team) => {
     let player = team.players.find(player => player.extra_player === true)
     // debugger
-    return <p>XH: {player.name}</p>
+    return (
+      <div>
+        <p>XH: {player.name}</p>
+        <p>Points: {this.renderScore(player)}</p>
+        <p class="meta">
+        Hits: {player.game_hits} |
+        Doubles: {player.game_doubles} |
+        Triples: {player.game_triples} |
+        HR: {player.game_home_runs} |
+        RBI: {player.game_rbi} |
+        Runs: {player.game_runs} |
+        Walks: {player.game_walks}
+        </p>
+      </div>
+    )
   }
 
 
